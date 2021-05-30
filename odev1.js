5fecd304fabcc147520ae88b3b1c1946abefac31
Muhammet Hadi KAMAT <muhammedhhadikamat@gmail.com>
2021-05-30 21:51:18 +0300

    edit

--- odev1.js
+++ odev1.js
@@ -1,5 +1,4 @@
 const radiusConsole=process.argv.slice(2);
-console.log(radiusConsole);
 function circleArea(radius){ 
     const area=Math.PI*(radius**2);
     console.log(`Yarıçapı ${radius} olan dairenin alanı: ${area.toFixed(2)}`);
